332ff473fe187a1e204e3e0d0e1478a4c263ce56
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2017-04-07 16:37:23 +0200

    wrote rudimentary main

--- src/Hsudoku.hs
+++ src/Hsudoku.hs
@@ -1,3 +1,17 @@
+module Main where
+
+import           Control.Monad
+import           Sudoku.Loader
+import           Sudoku.Solver
+import           Sudoku.Type
+
 main :: IO ()
-main = putStrLn "Write the damn program, mate!"
+main = do maybeSudoku <- loadSudoku Evil
+          putStrLn "Unsolved:"
+          mapM_ (putStrLn . show) maybeSudoku
+          putStrLn "Solved:"
+          let maybeSolutions = join $ fmap solveSudoku maybeSudoku
+          let maybeSolution  = fmap head maybeSolutions
+          mapM_ (putStrLn . show) maybeSolution
+          pure ()
 
