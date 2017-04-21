405c86b5e7dfd5eee4a09a131a1ebf8d2438e81e
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2017-04-21 18:04:29 +0200

    continued with ui

--- src/Hsudoku.hs
+++ src/Hsudoku.hs
@@ -1,45 +1,33 @@
 module Main where
 
 import           Data.GI.Base
+import qualified Data.Text     as T
 import qualified GI.Gtk        as Gtk
-import qualified GtkUtils      as Gtk
-
-import qualified Data.Text as T
 import           Sudoku.Loader
 import           Sudoku.Solver
 import           Sudoku.Type
+import qualified UserInterface as Ui
 import           Util
 
 main :: IO ()
 main = do
     Gtk.init Nothing
-    (window, builder) <- Gtk.buildMainWindow "mainWindow" "gui/hsudoku.ui"
-    Gtk.windowAddCss window "gui/theme.css"
-    inputPopover <- Gtk.builderGetTyped builder "inputPopover" Gtk.Popover
-    cells <- Gtk.builderGetsTyped builder cellNames Gtk.Button
-    cellsBindHandlers cells inputPopover
+
+    (window, builder) <- Ui.buildMainWindow "mainWindow" "gui/hsudoku.ui"
+    inputPopover <- Ui.builderGetTyped builder "inputPopover" Gtk.Popover
+
+    cells <- Ui.builderGetsTyped builder Ui.cellNames Gtk.Button
+    Ui.cellsBindHandlers cells inputPopover
+
+    numberButtons <- Ui.builderGetsTyped builder Ui.numberNames Gtk.Button
+    Ui.numbersBindHandlers numberButtons inputPopover
+    inputClear <- Ui.builderGetTyped builder "inputClear" Gtk.Button
+    on inputClear #clicked $ Ui.writePopoverRelativeCell inputPopover $ blankval
+
+    Just sudoku <- loadSudoku Easy
+    Ui.writeSudoku cells sudoku
+
     #showAll window
     Gtk.main
 
-cellNames :: [T.Text]
-cellNames = map (T.pack . (++) "cell") $ map show [1..81]
-
-cellsBindHandlers :: [Gtk.Button] -> Gtk.Popover -> IO ()
-cellsBindHandlers cells popover = mapM_ (\c -> do
-            on c #focusInEvent $ focusInHandler c
-            on c #focusOutEvent $ focusOutHandler c
-        ) cells
-    where focusInHandler c _ = cellShowPopover c popover
-          focusOutHandler c _ = maybeHidePopover popover
-
-cellShowPopover :: Gtk.Button -> Gtk.Popover -> IO Bool
-cellShowPopover cell popover = do
-    popover `set` [#relativeTo := cell]
-    #show popover
-    pure False
-
-maybeHidePopover :: Gtk.Popover -> IO Bool
-maybeHidePopover popover = do
-    -- #hide popover
-    pure False
 
---
+++ src/UserInterface.hs
@@ -0,0 +1,129 @@
+{-|
+Module: UserInterface
+Description: Provides various functions for building the user interface
+Copyright: (c) Marcel Moosbrugger, 2017
+License     : MIT
+
+Provides various functions for building the user interface of the
+sudoku application
+-}
+module UserInterface (
+      BuilderCastException(..)
+    , cellNames
+    , numberNames
+    , builderGetTyped
+    , builderGetsTyped
+    , buildMainWindow
+    , windowAddCss
+    , writeCell
+    , writePopoverRelativeCell
+    , cellsBindHandlers
+    , numbersBindHandlers
+    , writeSudoku
+    ) where
+
+import           Control.Exception
+import           Control.Monad.IO.Class
+import           Data.GI.Base
+import qualified Data.Text              as T
+import           Data.Typeable
+import           GI.Gtk
+import           Sudoku.Type
+
+-- | Thrown when 'castB' fails get an object
+data BuilderCastException = UnknownIdException String deriving (Show, Typeable)
+
+instance Exception BuilderCastException
+
+-- | The ids of the sudoku cells in the ui file
+cellNames :: [T.Text]
+cellNames = map (T.pack . (++) "cell") $ map show [1..81]
+
+-- | The ids of the inputs for the numbers in the ui file
+numberNames :: [T.Text]
+numberNames = map (T.pack . (++) "input") $ map show [1..9]
+
+-- | Takes a builder and returns the object with a given name
+--   typed as a given gtype
+builderGetTyped :: (IsBuilder a, GObject o, MonadIO m) => a -> T.Text -> (ManagedPtr o -> o) -> m o
+builderGetTyped builder ident gtype =
+    liftIO $ do
+        o <- builderGetObject builder ident
+        case o of
+            Just a  -> unsafeCastTo gtype a
+            Nothing -> throw $ UnknownIdException $ T.unpack ident
+
+-- | Same as builderGetTyped for a list of names
+builderGetsTyped :: (GObject a, IsBuilder b, MonadIO m) => b -> [T.Text] -> (ManagedPtr a -> a) -> m [a]
+builderGetsTyped b is t = sequence $ map (\i -> builderGetTyped b i t) is
+
+-- | Builds the main application window from a xml definition file for which the
+--   path is given
+buildMainWindow :: MonadIO m => T.Text -> T.Text -> m (Window, Builder)
+buildMainWindow name path = liftIO $ do
+    builder <- builderNewFromFile path
+    window  <- builderGetTyped builder name Window
+    on window #destroy mainQuit
+    windowAddCss window "gui/theme.css"
+    pure (window, builder)
+
+-- | Adds to a given window a css file for which the path is given
+windowAddCss :: (MonadIO m, IsWindow a) => a -> T.Text -> m ()
+windowAddCss window path = liftIO $ do
+    screen <- windowGetScreen window
+    cssProvider <- cssProviderNew
+    cssProviderLoadFromPath cssProvider path
+    styleContextAddProviderForScreen screen cssProvider 1000
+
+-- | Writes a character into a sudoku cell
+writeCell :: (IsBin o) => o -> Char -> IO ()
+writeCell cell char = do
+    binCell <- toBin cell
+    labelO <- binGetChild binCell
+    label <- unsafeCastTo Label labelO
+    labelSetText label (T.singleton char)
+
+-- | Writes a charachter into a cell which is associated to a given popover
+--   The popover gets closed afterwards.
+writePopoverRelativeCell :: Popover -> Char -> IO ()
+writePopoverRelativeCell popover char = do
+    widget <- #getRelativeTo popover
+    cell   <- unsafeCastTo Button widget
+    writeCell cell char
+    #hide popover
+
+-- | Binds the signal handlers to buttons
+cellsBindHandlers :: [Button] -> Popover -> IO ()
+cellsBindHandlers cells popover = mapM_ (\c -> do
+            on c #focusInEvent $ focusInHandler c
+        ) cells
+    where focusInHandler c _ = do cellShowPopover c popover; pure False
+
+-- | Associates the popover to a given button and shows the popover
+cellShowPopover :: Button -> Popover -> IO ()
+cellShowPopover cell popover = do
+    popover `set` [#relativeTo := cell]
+    #show popover
+
+-- | Binds the signal handlers to a list of number buttons
+numbersBindHandlers :: [Button] -> Popover -> IO ()
+numbersBindHandlers buttons popover = mapM_ (\b -> do
+            on b #clicked $ numberButtonInsert b popover
+        ) buttons
+
+-- | Inserts the content of a number button to a cell associated to the popover
+numberButtonInsert :: Button -> Popover -> IO ()
+numberButtonInsert button popover = do
+    label <- #getLabel button
+    writePopoverRelativeCell popover $ T.head label
+
+writeSudoku :: [Button] -> Sudoku -> IO ()
+writeSudoku buttons sudoku = do
+    let sudokuChars = toString sudoku
+    sequence_ $ zipWith (\b c -> do
+            writeCell b c
+            if c == blankval
+                then b `set` [#sensitive := True]
+                else b `set` [#sensitive := False]
+        ) buttons sudokuChars
+
