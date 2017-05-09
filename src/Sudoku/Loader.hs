04f1a280aaa628f11dc91e191dfaa1a2c6c6fa98
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2017-05-09 16:28:24 +0200

    Changed loader

--- src/Sudoku/Loader.hs
+++ src/Sudoku/Loader.hs
@@ -8,28 +8,40 @@ This module contains functions to load sudokus from the network
 -}
 module Sudoku.Loader(loadSudoku) where
 
-import           Data.List         (elemIndex)
-import           Data.Maybe        (fromMaybe)
+import           Data.ByteString.Lazy.Char8 (unpack)
+import           Data.List                  (elemIndex, transpose)
+import           Data.Maybe                 (fromMaybe)
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
 import           Sudoku.Type
 import           Text.HandsomeSoup
-import           Text.Printf       (printf)
+import           Text.Printf                (printf)
 import           Text.XML.HXT.Core
+import           Util
 
--- |The url pattern of the site from which the sudokus get loaded
-urlPattern :: String
-urlPattern = "http://show.websudoku.com/?level=%d"
+-- | Converts a difficulty to number which can be plugged into the url
+toNumber :: Difficulty -> Int
+toNumber Easy   = 40
+toNumber Medium = 33
+toNumber Hard   = 26
+toNumber Evil   = 17
 
--- |Creates the url from the url pattern and the difficutlty level
+-- | Creates the url for a given difficullty level from which the sudokus get
+-- loaded
 url :: Difficulty -> String
-url d = printf urlPattern $ param d
-    where param :: Difficulty -> Int
-          param d = fromMaybe 1 $ elemIndex d [Easy, Medium, Hard, Evil]
+url d = "https://kjell.haxx.se/sudoku/?visade=" ++ (show $ toNumber d) ++ "&seed=%28random+seed%29&action=Create+a+field&hardchange=0"
 
 -- |Loads a sudoku from the network with a given difficulty level
 loadSudoku :: Difficulty -> IO (Maybe Sudoku)
 loadSudoku d = do
-    let doc = fromUrl (url d)
-    values <- runX $ doc >>> css "#puzzle_grid input" ! "value"
-    let sudokuString = concat $ map (\v -> if v == "" then blankval:"" else v) values
+    manager <- newManager tlsManagerSettings
+    request <- parseRequest (url d)
+    response <- httpLbs request manager
+    let doc = parseHtml $ unpack $ responseBody response
+    values <- runX $ doc >>> css "input.sgrid" ! "value"
+    -- in the html the values are aranged block wise and not row wise
+    let transposedValues = ungroup . ungroup . transpose . groupBy 3 . groupBy 9
+                         . ungroup . ungroup . transpose . groupBy 3 . groupBy 3 $ values
+    let sudokuString = concat $ map (\v -> if v == "" then blankval:"" else v) transposedValues
     pure (fromString sudokuString)
 
